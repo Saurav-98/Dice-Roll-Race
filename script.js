f8c8f3b562439f372512c3b4050fc0efeeab5b91
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-25 21:48:26 +0530

    Complete refactor Game Logic

--- script.js
+++ script.js
@@ -1,86 +1,67 @@
 'use strict';
 
-// Selecting the DOM elements to Maipulate
+// ************************************************************ DOM Elements **********************************************************
 
 // Final Scores of Both Players
 
 const scorePlayer0 = document.getElementById('score--0');
 const scorePlayer1 = document.getElementById('score--1');
-
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-
 const playersEl = document.querySelectorAll('.player');
 
 // Dice Image
-
 const diceImg = document.querySelector('.dice');
 
 // DOM Elements for RollDice Function
-
 const rollBtn = document.querySelector('.btn--roll');
 const playersCurrentScore = document.querySelectorAll('.current-score');
+
+// DOM elements for Hold function
+const holdBtn = document.querySelector('.btn--hold');
+const displayFinalScores = document.querySelectorAll('.score');
+const winnerBoxEl = document.querySelector('.winner-box');
+const newGameBtn = document.querySelector('.btn--new');
+
+// ********************************** Variables *****************************************
+
 let activePlayer = 0;
-// let otherPlayer = activePlayer === 0 ? 1 : 0;
-
-// Variables
-
 const finalScores = [0, 0];
-
 let currentScore = 0;
 
-// Setting Initial Score Values to 0
+// ************************************************************ All Game Functions **********************************************************
 
-scorePlayer0.textContent = 0;
-scorePlayer1.textContent = 0;
-diceImg.classList.add('hidden');
-
-// Creating Dice Roll Function
+// ******************** 1 - Dice Roll Function ************************
 
 const diceRoll = () => {
   // 1. Generate random dice value
-
   const diceRollVal = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceRollVal);
   // 2. Display Dice Image
   diceImg.src = `./dice-${diceRollVal}.png`;
   // Remove Hide class from Dice Img
   diceImg.classList.remove('hidden');
   // 3. Check Is it a 1
-
   if (diceRollVal !== 1) {
     // 4. Add Dice value to current Score
     currentScore += diceRollVal;
     // 5. Display new current Value
     playersCurrentScore[activePlayer].textContent = currentScore;
   } else {
-    // Set CurrentScore to 0
-    console.log(`Player Out at ${currentScore}`);
-
     // Switch Player
     switchPlayers();
   }
 };
 
-rollBtn.addEventListener('click', diceRoll);
-
-// DOM elements for Hold function
-
-const holdBtn = document.querySelector('.btn--hold');
-const displayFinalScores = document.querySelectorAll('.score');
-
-// Creating Score Hold Function
+// ******************** 2. Hold Function ************************
 
 const holdScore = () => {
   // Add current Score to Final Score
   finalScores[activePlayer] += currentScore;
   // Display Final score
-  // console.log(displayFinalScores);
   displayFinalScores[activePlayer].textContent = finalScores[activePlayer];
-
   // Check if finalScore >= 100 to End  the Game
-
-  if (finalScores[activePlayer] >= 12) {
+  if (finalScores[activePlayer] >= 100) {
+    // Game Over
     gameOver();
   } else {
     // Switch Player
@@ -88,48 +69,79 @@ const holdScore = () => {
   }
 };
 
-holdBtn.addEventListener('click', holdScore);
-
-const winnerBoxEl = document.querySelector('.winner-box');
+// ******************** 3. Switch Players Function ************************
 
 const switchPlayers = () => {
+  // setting current Value to 0
   currentScore = 0;
+  // Display Current Value
   playersCurrentScore[activePlayer].textContent = currentScore;
+  // Switch Active Player
   activePlayer = activePlayer === 0 ? 1 : 0;
+  // Toggle Active Player Class for the Overlay Highlight
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
 
+// ******************** 4. Game Over Function ************************
+
 const gameOver = () => {
+  // adding winner class to Active player and loser class to the other player for Styling
   playersEl[activePlayer].classList.add('winner');
   let otherPlayer = activePlayer === 0 ? 1 : 0;
   playersEl[otherPlayer].classList.add('loser');
+
+  // Showing the Winner Message
   winnerBoxEl.textContent = `Player ${activePlayer + 1} Wins The Game.`;
   winnerBoxEl.style.display = 'block';
+
+  // Hide Dice Image
   diceImg.classList.add('hidden');
+
+  // Hiding Play Buttons
   rollBtn.style.visibility = 'hidden';
   holdBtn.style.visibility = 'hidden';
 };
 
-const newGameBtn = document.querySelector('.btn--new');
+// ******************** 5. Game Reset Function ************************
 
 const resetToStart = () => {
+  // ****************** Resetting All Values to Initial Values and removing additional classes added for styling *****************************************
+  // Determining Active Player( Winner ) and Other Player ( Loser ) and removing styles
   let otherPlayer = activePlayer === 0 ? 1 : 0;
   playersEl[activePlayer].classList.remove('winner');
   playersEl[otherPlayer].classList.remove('loser');
+
+  // Hiding Winner Message and Displaying Back the Game Buttons
   winnerBoxEl.style.display = 'none';
   rollBtn.style.visibility = 'visible';
   holdBtn.style.visibility = 'visible';
+
+  // Setting all Values to initial Values
   finalScores[0] = 0;
   finalScores[1] = 0;
-  scorePlayer0.textContent = 0;
-  scorePlayer1.textContent = 0;
   currentScore = 0;
+
+  // Displaying Finals Scores and Current Scores for both players to 0
   playersCurrentScore[activePlayer].textContent = 0;
   playersCurrentScore[otherPlayer].textContent = 0;
+  scorePlayer0.textContent = 0;
+  scorePlayer1.textContent = 0;
+
+  // Adding "Player---active" class to player 1 & Setting Active Player to 0
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
   activePlayer = 0;
 };
 
+// ************************************************************ Game Execution Steps **********************************************************
+
+// Setting Initial Score Values to 0
+
+scorePlayer0.textContent = 0;
+scorePlayer1.textContent = 0;
+diceImg.classList.add('hidden');
+
+rollBtn.addEventListener('click', diceRoll);
+holdBtn.addEventListener('click', holdScore);
 newGameBtn.addEventListener('click', resetToStart);
